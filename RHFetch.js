4caf58f93589ded1d588a3d61538b13b6ec56cdf
methodcase <methodcase@gmail.com>
2016-09-29 08:03:50 +0800

    Update RHFetch

--- RHFetch.js
+++ RHFetch.js
@@ -1,13 +1,18 @@
 var RHFetch = (function() {
+    
     this.get = (url) =>{
         this.url = url
         return this
     }
     this.result = (cb) => {
-        fetch(this.url)
-        .then(r => r.json())
-        .then(r => cb(r))
-        return this
+        try{
+            fetch(this.url)
+            .then(r => r.json())
+            .then(r => cb(r))
+            return this
+        }catch(e){
+            return e
+        }                
     }
 })
 
