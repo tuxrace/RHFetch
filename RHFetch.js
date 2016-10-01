9fbd1f9fde2df31171726dc46dc3b1a86a3225b8
methodcase <methodcase@gmail.com>
2016-10-01 10:11:38 +0800

    add options

--- RHFetch.js
+++ RHFetch.js
@@ -1,19 +1,38 @@
 var RHFetch = (function() {
-    
+    const FetchOptions = {
+        method:'GET',
+        headers: () => {
+            return new Headers()
+        },
+        mode: 'cors',
+        cache:'default'
+    }
+    this.opts = FetchOptions
+    this.version = 0.1 
+
     this.get = (url) =>{
+        FetchOptions.method = 'GET'
+        this.opts = FetchOptions
         this.url = url
         return this
     }
-    this.result = (cb) => {
-        try{
-            fetch(this.url)
-            .then(r => r.json())
-            .then(r => cb(r))
-            return this
-        }catch(e){
-            return e
-        }                
+    
+    this.post = () => {
+        FetchOptions.method = 'POST'
+        this.opts = FetchOptions
+        this.url = url
+        return this
     }
+    
+    this.result = (cb) => {                
+        fetch(this.url,this.opts)
+        .then(r => r.json())
+        .then(r => cb(r))
+        .catch(err=> {
+            console.log(err)
+        })                           
+        return this             
+    }    
 })
 
 window.RHFetch = new RHFetch();
